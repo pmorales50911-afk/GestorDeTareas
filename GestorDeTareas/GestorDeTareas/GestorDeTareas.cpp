089d0f29dd3fa8fce7066ea1e01abea5324971a3
pmorales50911-afk <pmorales50911@ucq.edu.mx>
2026-06-11 22:56:01 -0600

    codigo terminado
    Realice el codigo y lo termine

--- GestorDeTareas/GestorDeTareas/GestorDeTareas.cpp
+++ GestorDeTareas/GestorDeTareas/GestorDeTareas.cpp
@@ -1,19 +1,47 @@
-// GestorDeTareas.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+int main() {
+    string tarea1;
+    string tarea2;
+    string tarea3;
+    string tarea4;
+    int opcion;
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+    do {
+        cout << "Menu de tareas" << endl;
+        cout << "1 Agregar tareas" << endl;
+        cout << "2 Mostrar tareas" << endl;
+        cout << "3 Salir" << endl;
+        cout << "Elige una opcion:" << endl;
+        cin >> opcion;
+        cin.ignore();
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+        if (opcion == 1) {
+            cout << "Escribe la primera tarea:" << endl;
+            getline(cin, tarea1);
+
+            cout << "Escribe la segunda tarea: " << endl;
+            getline(cin, tarea2);
+
+            cout << "Escribe la tercera tarea:" << endl;
+            getline(cin, tarea3);
+
+            cout << "Escribe la cuarta tarea:" << endl;
+            getline(cin, tarea4);
+
+            cout << "Tareas guardadas" << endl;
+        }
+        else if (opcion == 2) {
+            cout << "Tus tareas son ";
+            cout << "1 " << tarea1 << endl;
+            cout << "2 " << tarea2 << endl;
+            cout << "3 " << tarea3 << endl;
+            cout << "4 " << tarea4 << endl;
+        }
+    } while (opcion != 3);
+
+    cout << "Hasta luego" << endl;
+    return 0;
+};
